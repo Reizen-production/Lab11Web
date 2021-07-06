b591816d53a5b443d51f58fa241c7697e7fa7e69
Reizen-production <56240091+Reizen-production@users.noreply.github.com>
2021-07-06 11:57:26 +0700

    Add files via upload

--- Controllers/Artikel.php
+++ Controllers/Artikel.php
@@ -32,9 +32,15 @@ class Artikel extends BaseController
     public function admin_index()
     {
         $title = 'Daftar Artikel';
+        $q = $this->request->getVar('q') ?? '';
         $model = new ArtikelModel();
-        $artikel = $model->findAll();
-        return view('artikel/admin_index', compact('artikel', 'title'));
+        $data = [
+            'title' => $title,
+            'q' => $q,
+            'artikel' =>  $model->like('judul', $q)->paginate(5), #data dibatasi 5 record per halaman
+            'pager' => $model->pager,
+            ];
+        return view('artikel/admin_index', $data);           
     }
     public function add()
     {
@@ -45,11 +51,15 @@ class Artikel extends BaseController
     
         if ($isDataValid)
         {
+            $file = $this->request->getFile('gambar');
+            $file->move(ROOTPATH . 'public/gambar');
+
             $artikel = new ArtikelModel();
             $artikel->insert([
                 'judul' => $this->request->getPost('judul'),
                 'isi' => $this->request->getPost('isi'),
                 'slug' => url_title($this->request->getPost('judul')),
+                'gambar' => $file->getName()
             ]);
             return redirect('admin/artikel');
         }
